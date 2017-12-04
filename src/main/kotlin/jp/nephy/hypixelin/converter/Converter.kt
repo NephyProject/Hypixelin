9467e873eeac80132daedef1905991a448256862
Nep <spica@starry.blue>
2017-12-04 16:05:38 +0900

    Add: Json converter

---
+++ src/main/kotlin/jp/nephy/hypixelin/converter/Converter.kt
@@ -0,0 +1,8 @@
+package jp.nephy.hypixelin.converter
+
+import com.github.salomonbrys.kotson.obj
+import com.google.gson.JsonElement
+
+internal inline fun <reified R, reified T> JsonElement.byConverter(key: String?=null) = JsonConvertDelegate(T::class.java, R::class.java, this.obj, key)
+internal inline fun <reified T> JsonElement.byModel(key: String?=null) = this.byConverter<JsonElement, T>(key)
+internal inline fun <reified T> JsonElement.byList(key: String?=null, noinline converter: ((JsonElement) -> Any)={ it }) = JsonConvertArrayDelegate(T::class.java, this.obj, key, converter)
